bd2f290d2429b40db18ed99c415b238afd88ca42
unknown <ray>
2024-03-11 21:44:20 +0800

    update clear.py

--- clear.py
+++ clear.py
@@ -10,6 +10,11 @@ try:
     os.remove(PROJECT_DIR+'\\result.txt')
 except FileNotFoundError:   
     pass
+
+f=open(PROJECT_DIR+'\\setting.txt','w')
+f.write("mapName=\nblockLength=\nblockWidth=")
+f.close()
+
 os.mkdir(PROJECT_DIR+'\\blocks')
 os.mkdir(PROJECT_DIR+'\\blocks\\event')
 os.mkdir(PROJECT_DIR+'\\blocks\\traversable')
